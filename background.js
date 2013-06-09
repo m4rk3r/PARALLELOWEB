81a020023c5ce5ec819a4980d4484f684564515d
m4rk3r <mbeasl1@gmail.com>
2013-06-09 16:12:21 -0500

    [ major revisions ] clingy-browsing, navigation away from current page funcationality

--- background.js
+++ background.js
@@ -36,27 +36,28 @@ function req(qstring,port){
     var xhr = new XMLHttpRequest();
     xhr.open("GET", qstring, true);
     xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-          port.postMessage( {data:JSON.stringify(xhr.responseText),kill:false} );
+      if ( xhr.readyState == 4 && port ) {
+          port.postMessage( {data:JSON.stringify(xhr.responseText)} );
       }
     }
     xhr.send();
 }
 
+var has_listeners = false;
 var page;
 var previous_tab;
 var active = false;
 var client_id;
 var db  = {};
 var clients_for_tabs = {}
+var client_id = Math.random().toString(36).substring(2);
 
 chrome.extension.onConnect.addListener(function (port) {
     port.onMessage.addListener( function(msg) {
-        active=true;
         if( msg.handshake ){
-            client_id = msg.client;
             db[ client_id ] = port;
             clients_for_tabs[ previous_tab ] = client_id;
+            port.postMessage( {init:true,client_id:client_id})
         }else{
             req( msg.url+page, port );
         }
@@ -64,35 +65,57 @@ chrome.extension.onConnect.addListener(function (port) {
 });   
 
 
+function on_exit(data){
+    if(active && previous_tab == data.tabId ){
+        try{
+            if( clients_for_tabs[ previous_tab ] ) db[ client_id ].postMessage( {kill:true} )
+        }catch(err){}
+    }   
+}
 
-chrome.browserAction.onClicked.addListener(function test(tab) { 
+function on_load(data){
+    if(active && previous_tab == data.tabId ){
+        chrome.tabs.executeScript(previous_tab, {file: "jquery-1.8.2.js" });
+        chrome.tabs.executeScript(previous_tab, {file: "paralleloweb.js" });
+    
+        var parsed = parseUri(data.url);
+        page = '&url='+parsed.host + (parsed.relative.length > 1?parsed.relative:'');
+    }
+}
+
+
+chrome.browserAction.onClicked.addListener(function (tab) { 
     if(!active){
-        chrome.tabs.executeScript(tab.id, {file: "jquery-1.8.2.js"});
-        chrome.tabs.executeScript(tab.id, {file: "paralleloweb.js"});
+        if(!has_listeners){
+            has_listeners=true;
+            chrome.webNavigation.onBeforeNavigate.addListener( on_exit );
+            chrome.webNavigation.onCompleted.addListener( on_load );
+        }
+        
+        // activate icon
+        chrome.browserAction.setIcon({path: 'iconactive.png'});
+        
+        chrome.tabs.executeScript(tab.id, {file: "jquery-1.8.2.js" });
+        chrome.tabs.executeScript(tab.id, {file: "paralleloweb.js" });
         var parsed = parseUri(tab.url);
         page = '&url='+parsed.host + (parsed.relative.length > 1?parsed.relative:'');
         previous_tab = tab.id;
+        active = true;
     }else{
-        // kill old interval, start new one!
+        // turn off
+        active = false;
+        chrome.browserAction.setIcon({path: 'icon.png'});
         try{
             if( clients_for_tabs[ previous_tab ] ) db[ client_id ].postMessage( {kill:true} )
-        }catch(err){
-            //console.log('error connecting to port');
-        }
-        page = '&url='+parseUri(tab.url).host;
-        previous_tab = tab.id;
-        setTimeout(function (){
-            chrome.tabs.executeScript(tab.id, {file: "jquery-1.8.2.js"});
-            chrome.tabs.executeScript(tab.id, {file: "paralleloweb.js"});  
-        },500)
+        }catch(err){}
     }
 });
 
 
 chrome.tabs.onRemoved.addListener(function(tabId){
     if(active && tabId == previous_tab){
+        chrome.browserAction.setIcon({path: 'icon.png'});
         clients_for_tabs[ previous_tab ]=false;
-        //console.log("REMOVING LISTENER");
         active=false;
     }
 });
--- paralleloweb.js
+++ paralleloweb.js
@@ -1,5 +1,6 @@
 var mice_bin = new Array();
-var client_id = Math.random().toString(36).substring(2);
+
+var client_id;
 var living=[];
 var available=true;
 var pos = new Object();
@@ -9,24 +10,27 @@ var count = 0;
 var kill = false;
 var URL = new Object();
 URL.base = 'http://duskjacket.com/MMORPP/';
+//URL.base = 'http://localhost:8000/MMORPP/';
 URL.update = URL.base + 'get-and-update-json/';
 
 
 var port = chrome.extension.connect({name: "datapipe-"+client_id});  
-port.postMessage({handshake:true,client:client_id}); 
+port.postMessage({handshake:true}); 
 
 function mk_req( _url ){
     available=false;
-    port.postMessage({url:_url,client:client_id,handshake:false});   
+    port.postMessage({url:_url,client:client_id});   
 }
 
 port.onMessage.addListener(function(msg) {
     if( msg.kill ){
         kill = true;
+    }else if( msg.init ){
+        client_id = msg.client_id;
     }else{
         var data = JSON.parse(JSON.parse( msg.data ));
         process_response( data );
-        available = true;
+        available = true;   
     }
 });
 
@@ -45,7 +49,12 @@ function process_response( data ){
         living.push( key );
         if(key == client_id) continue // ourself
         if(mice_bin.indexOf(key) != -1){
-            mice_bin[key].img.css({ 'left': clients[i][1][0], 'top':clients[i][1][1] });
+            if( clients[i][1][2] ){ // moved flag
+                mice_bin[key].img.attr('src',chrome.extension.getURL('pointer.gif'));
+                mice_bin[key].img.wrap("<a href='"+clients[i][1][3]+"' />");
+            }else{
+                mice_bin[key].img.css({ 'left': clients[i][1][0], 'top':clients[i][1][1] });   
+            }
         }else{
             var cursor = $("<img src='"+chrome.extension.getURL('pointer.png')+"' class='mouse-pointer' >");
             cursor.css({'left':clients[i][1][0],'top':clients[i][1][1],'position':'absolute','width': 10,'z-index':30})
@@ -73,6 +82,7 @@ interval = setInterval(function (){
         }
     }
     if(kill){
+      $('.mouse-pointer').each(function (){ $(this).fadeOut(500) });
       clearInterval(interval);  
     } 
 },30)
